ded34fd9aa7799e2d8ad08fc5ce7565962c1467d
David <bensadondavidn@gmai.com>
2025-07-25 05:19:59 +0300

    changes

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -3,12 +3,15 @@ import Header from "../Components/Header"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { addList, clearList } from "../Store/listSlice"
+import InfosIcon from "../assets/icons/InfosIcon"
+import PlayIcons from "../assets/icons/PlayIcons"
 
 interface List {
   id: string;
   name: string;
   firstLanguage: string;
   secondLanguage: string;
+  lastReviewed : string
 }
 
 function Home() {
@@ -17,7 +20,10 @@ function Home() {
   const state = useSelector(state => state.list)
 
   const urlBack = '';
-  const [lists, setLists] = useState<List[]>([]);
+  const [lists, setLists] = useState<List[]>([
+    {id : '1', name : 'work', firstLanguage : 'english', secondLanguage : 'french', lastReviewed : '28/02/2025' },
+    {id : '1', name : 'work', firstLanguage : 'english', secondLanguage : 'french', lastReviewed : '28/02/2025' },
+  ]);
   const [selectedListId, setSelectedListId] = useState('');
   const [firstLanguage, setFirstLanguage] = useState('');
   const [secondLanguage, setSecondLanguage] = useState('');
@@ -105,10 +111,21 @@ function Home() {
 
           <div className="home-lists">
             <p className="home-lists-title">Your Lists</p>
+            <div className="home-lists-list-titles">
+              <p>Name</p>
+              <p>Languages</p>
+              <p style={{transform : 'translateX(22px)'}}>Last practice</p>
+              <p>Infos / Play</p>
+            </div>
             {lists.map((list)=>(
               <div className="home-lists-list" key={list.id}>
                 <p>{list.name}</p>
-                <p>{list.name}</p>
+                <p>{list.firstLanguage} / {list.secondLanguage}</p>
+                <p>{list.lastReviewed}</p>
+              <div className="home-lists-buttons">
+              <button><InfosIcon /></button>
+              <button><PlayIcons /></button>
+              </div>
               </div>
             ))}
           </div>
