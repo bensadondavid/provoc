beae46e92a5342594dab12187818f41def0596d5
David <bensadondavidn@gmai.com>
2025-08-05 19:43:39 +0300

    changes

--- client/src/Pages/LaunchGame.tsx
+++ client/src/Pages/LaunchGame.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "../Components/Header"
 import { useLocation } from "react-router-dom";
 
@@ -44,7 +44,13 @@ function LaunchGame() {
   };
 
   const [list, setList] = useState<List>()
+  const [word, setWord] = useState<Word | null>(null)
+  const wordRef = useRef<HTMLInputElement>(null)
   const [words, setWords] = useState<Word[]>([])
+  const [completedWords, setCompletedWords] = useState<Word[]>([])
+  const [score, setScore] = useState<number>(0)
+  const [total, setTotal] = useState<number>(0)
+  const [errorMessage, setErrorMessage] = useState<string>('')
 
   const getWords = async()=>{
     try{
@@ -65,24 +71,85 @@ function LaunchGame() {
     }
   }
 
+  const [firstLang, setFirstLang] = useState<"firstLanguage" | "secondLanguage" | null>(null)
+  const [secondLang, setSecondLang] = useState<"firstLanguage" | "secondLanguage" | null>(null)
+
+  const chooseLanguage = ()=>{
+    if (firstLanguage && secondLanguage && list?.firstLanguage) {
+      if(list?.firstLanguage === firstLanguage){
+        setFirstLang("firstLanguage")
+        setSecondLang("secondLanguage")
+      }
+      else if(list?.firstLanguage === secondLanguage){
+        setFirstLang("secondLanguage")
+        setSecondLang("firstLanguage")
+      }
+    }
+  }
+
   useEffect(()=>{
     getWords()
   },[])
 
+  useEffect(() => {
+  if (list && firstLanguage && secondLanguage) {
+    chooseLanguage();
+  }
+}, [list, firstLanguage, secondLanguage]);
+
+const chooseWord = ()=>{
+  if(!firstLang || completedWords.length === words.length){
+    return null
+  }
+  let index = Math.floor(Math.random()*words.length)
+  let candidate : Word = words[index]
+  while(completedWords.some(w => w[firstLang] === candidate[firstLang])){
+    index = Math.floor(Math.random()*words.length)
+    candidate = words[index]
+  }
+  const newWord = words[index]
+  setWord(newWord)
+}
+
+useEffect(() => {
+  if (words.length > 0 && firstLang) {
+    chooseWord();
+  }
+}, [words, firstLang]);
+
+
+const handleSubmit = (e : React.FormEvent<HTMLFormElement>)=>{
+  e.preventDefault()
+  if(wordRef.current && wordRef.current.value.trim().toLowerCase() === word[secondLang].trim().toLowerCase()){
+    setCompletedWords(prev=> [...prev, word])
+    setScore(s => s + 1)
+    setTotal(t => t + 1)
+    chooseWord()
+    setErrorMessage('')
+    wordRef.current.value = ''
+    return
+  }
+  chooseWord()
+  setTotal(t => t + 1)
+  wordRef.current.value = ''
+}
 
   return (
     <>
     <Header />
     <div className="launch-game">
       <div className="launch-game-container">
-        <p className="word-to-translate">{words[0].firstLanguage}</p>
-        <form>
-          <input type="text" />
-          <button>Submit</button>
+        <p className="word-to-translate">{firstLang && word && word[firstLang]}</p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="wordSecondLanguage" ref={wordRef} />
+          <button type="submit">Submit</button>
         </form>
         <div className="score-timer">
-          <p className="score">Score</p>
+          <p className="score">{score}/{total}</p>
           <p>{formatTime(elapsed)}</p>
+          {errorMessage &&
+          (<p style={{textAlign : 'center', color : '#e409ef', fontSize : 'large', marginTop : '50px'}}>{errorMessage}</p>)
+          }
         </div>
       </div>
     </div>
